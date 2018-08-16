54bac37dbf523afe45781f4c37d1d1e05687c35b
webdevabhi <abhishek.patel185@gmail.com>
2018-08-16 12:27:40 +0530

    Express Setup

---
+++ src/client/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <div>I'm the home component</div>
+            </div>
+        );
+    }
+}
+
+export default Home;
