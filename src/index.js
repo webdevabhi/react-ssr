f95b40bd093737e75d84e99249d22a12af8cac07
webdevabhi <abhishek.patel185@gmail.com>
2018-08-16 15:31:02 +0530

    Client Bootup

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
             </head>
 
             <body>
-                <div>${content}</div>
+                <div id="root">${content}</div>
                 <script src="bundle.js"></script>
             </body>
         </html>
