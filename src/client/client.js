f70145c0a621f453245c1de5ccb40fe130e09f36
webdevabhi <abhishek.patel185@gmail.com>
2018-08-16 18:24:47 +0530

    Reducer Imports

--- src/client/client.js
+++ src/client/client.js
@@ -6,7 +6,7 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import Routes from "./Routes";
-import { SSL_OP_MICROSOFT_BIG_SSLV3_BUFFER } from 'constants';
+import reducers from "./reducers";
 
 
 const store = createStore(reducers, {}, applyMiddleware(thunk));
