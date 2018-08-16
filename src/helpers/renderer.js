5fde3ff4cf3abdfdf43a35e5ca07a838f039cec4
webdevabhi <abhishek.patel185@gmail.com>
2018-08-16 15:52:51 +0530

    Renderer Helper

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { renderToString } from "react-dom/server";
-import Home from "./client/components/Home";
+import Home from "../client/components/Home";
 
 export default () => {
     const content = renderToString(<Home />);
