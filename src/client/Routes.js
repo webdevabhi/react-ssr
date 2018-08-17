8965ecd854bca332957a991d5c34e39dce8370c8
webdevabhi <abhishek.patel185@gmail.com>
2018-08-17 12:24:08 +0530

    The React Router Config Library

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,13 +1,24 @@
 import React from "react";
-import { Route } from "react-router-dom";
+// import { Route } from "react-router-dom";
 import Home from "./components/Home";
 import UsersList from "./components/UsersList";
 
-export default () => {
+/* export default () => {
     return (
         <div>
             <Route exact path="/" component={Home} />
             <Route path="/users" component={UsersList} />
         </div>
     )
-};
+}; */
+
+export default [
+    {
+        path: '/',
+        component: Home,
+        exact: true
+    }, {
+        path: '/users',
+        component: UsersList
+    }
+];
