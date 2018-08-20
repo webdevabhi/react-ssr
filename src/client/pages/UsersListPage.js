48e6e7b2bb61e3d5bd16aa407e3c5040715ee881
webdevabhi <abhishek.patel185@gmail.com>
2018-08-20 11:48:33 +0530

    Proxy Setup

--- src/client/pages/UsersListPage.js
+++ src/client/pages/UsersListPage.js
@@ -5,7 +5,7 @@ import { fetchUsers } from "../actions";
 class UsersList extends Component {
 
     componentDidMount() {
-        this.props.fetchUsers();
+        // this.props.fetchUsers();
     }
 
     renderUsers() {
