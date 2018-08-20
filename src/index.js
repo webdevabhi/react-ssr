15de3e65937bf802c1296a8037452f07d5116dba
webdevabhi <abhishek.patel185@gmail.com>
2018-08-20 12:40:34 +0530

    Server Axios Instance

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ const app = express();
 
 app.use(
     '/api', 
-    proxy('http://react-sss-api.herokuapp.com', {
+    proxy('http://react-ssr-api.herokuapp.com', {
         proxyReqOptDecorator(opts) {
-            opts.header['x-forwarded-host'] = 'localhost:3000';
+            opts.headers['x-forwarded-host'] = 'localhost:3000';
             return opts;
         }
     })
@@ -20,7 +20,7 @@ app.use(
 
 app.use(express.static('public'));
 app.get('*', (req, res) => {
-    const store = createStore();
+    const store = createStore(req);
 
     const promises = matchRoutes(Routes, req.path).map(({ route }) => {
         return route.loadData ? route.loadData(store) : null;
