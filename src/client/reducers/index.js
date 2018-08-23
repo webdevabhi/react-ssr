b7d2eaa656da2f69b9e06ed08d8b5b2dd104f580
webdevabhi <abhishek.patel185@gmail.com>
2018-08-23 11:31:30 +0530

    Fetching Auth Status

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import usersReducer from "./usersReducer";
+import authReducer from "./authReducre";
 
 export default combineReducers({
-    users: usersReducer
+    users: usersReducer,
+    auth: authReducer
 });
